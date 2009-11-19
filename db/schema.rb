62a997b98e166962c47a03390fc28fd247dcfe6e
Scott Thorpe <sthorpe@gmail.com>
2009-11-19 15:14:58 -0800

    Removing article migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091113184925) do
+ActiveRecord::Schema.define(:version => 20091119191436) do
+
+  create_table "articles", :force => true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", :force => true do |t|
     t.string   "name"
